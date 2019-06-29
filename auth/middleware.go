1d2c06b17561667bc974ac31dc1851dd4eb8e091
skyoo2003 <skyoo2003@gmail.com>
2019-06-29 20:15:05 +0900

    인증 미들웨어에 회원 여부 체크 추가

--- auth/middleware.go
+++ auth/middleware.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 	"github.com/itslaves/rentalgames-server/common/sessions"
@@ -12,24 +13,26 @@ import (
 func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Session(c)
-		if _, exist := session.Values["userID"]; !exist {
-			webAddr := viper.GetString("web.addr")
-			location := fmt.Sprintf("http://%s/login", webAddr)
-			c.Redirect(http.StatusFound, location)
-		}
-		/*
-			else {
-				r, err = db.Select("sdadad wher uid = %s", uid)
-				if err != nil {
-					params : url.Values{}
-					params["email"] = session.Values["email"]
+		webAddr := viper.GetString("web.addr")
+		if userID, ok := session.Values[UserID]; ok {
+			// TODO: MySQL DB 연동 부분
+			var err error // FIXME: DB 연동 후 제거
+			// _, err := db.Select(fmt.Sprintf("SELECT userID FROM users WHERE userID = '%s'", userID))
+			if err != nil {
+				params := url.Values{}
+				params.Set(UserID, userID.(string))
+				params.Set(Nickname, session.Values[Nickname].(string))
+				params.Set(ProfileImage, session.Values[ProfileImage].(string))
+				params.Set(Gender, session.Values[Gender].(string))
+				params.Set(Email, session.Values[Email].(string))
 
-					webAddr := viper.GetString("web.addr")
-					location := fmt.Sprintf("http://%s/join?%s", webAddr, params.Encode())
-					c.Redirect(http.StatusFound, location)
-				}
+				joinPage := fmt.Sprintf("http://%s/join?%s", webAddr, params.Encode())
+				c.Redirect(http.StatusTemporaryRedirect, joinPage)
 			}
-		*/
+		} else {
+			loginPage := fmt.Sprintf("http://%s/login", webAddr)
+			c.Redirect(http.StatusTemporaryRedirect, loginPage)
+		}
 		c.Next()
 	}
 }
