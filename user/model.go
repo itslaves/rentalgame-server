538c5ee2369b2a9b0a7f1b67defafca3427bfeaa
skyoo2003 <skyoo2003@gmail.com>
2019-06-30 23:06:35 +0900

    칼럼 불일치 수정

--- user/model.go
+++ user/model.go
@@ -5,7 +5,7 @@ type User struct {
 	Email        string  `gorm:"type:char(11);not null;unique:email_UNIQUE"`
 	Nickname     string  `gorm:"type:char(20);not null"`
 	Gender       string  `gorm:"type:char(1);not null"`
-	ProfileImage *string `gorm:"type:char(100)"`
+	ProfileImage *string `gorm:"column:profile_img;type:char(100)"`
 	Age          int     `gorm:"type:int(11)"`
 	OAuthKakao   *string `gorm:"column:oauth.kakao;type:char(100)"`
 	OAuthNaver   *string `gorm:"column:oauth.naver;type:char(100)"`
